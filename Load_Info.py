64ecdee2fda68e428cbf0641c86b83aac6a87c04
Mayur Patel <123456+Mayur6306@users.noreply.github.com>
2025-11-03 17:02:50 -0600

    Updated get_courses and get_assignments functions

--- Load_Info.py
+++ Load_Info.py
@@ -4,33 +4,61 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#Local Institution's Canvas' Domain URL
+# Local Institution's Canvas' Domain URL
 CANVAS_URL = os.getenv("CANVAS_URL")
 
-#User's Canvas' Access Token
+# User's Canvas' Access Token
 CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
 
 
+# Returns User's Active course ids
 def get_courses(canvas_url: str, canvas_token: str) -> dict:
-    response = requests.get(
-    f"https://{canvas_url}/api/v1/courses?enrollment_state=active",
+
+    courses = requests.get(
+    f"https://{canvas_url}/api/v1/courses?enrollment_state=active&include[]=term",
     headers={"Authorization":f"Bearer {canvas_token}","Accept":"*/*"},
-    )
+    ).json()
 
-    return response.json()
+    courses_info = {course["name"]:course['id'] for course in courses}
 
-def get_assignements(canvas_url: str, course_id: int, canvas_token: str) -> dict:
-    response = requests.get(
-    f"https://{canvas_url}/api/v1/courses/{course_id}/assignments?include[]=submission?order_by=due_at?per_page=100",
-    headers={"Authorization":f"Bearer {canvas_token}","Accept":"*/*"},
-    )
-
-    return response.json()
+    return courses_info
 
 
+def get_assignments(canvas_url: str, courses: list[int], canvas_token: str) -> dict:
+
+    assignments = []
+
+    for course_name, course_id in courses.items():
+        
+        assigments_info = requests.get(
+        f"https://{canvas_url}/api/v1/courses/{course_id}/assignments?include[]=submission&order_by=due_at&per_page=100",
+        headers={"Authorization":f"Bearer {canvas_token}","Accept":"*/*"},
+        ).json()
+
+        
+        for info in assigments_info:
+
+
+            # Visible Columns[course_name, assignment_name, due_date, days_left, priority, status, submitted, notes, link,
+            # Hidden Hidden: .sync_id, .source, .due_date_utc, .content_hash, .created_at, .updated_at, .last_synced]
+            assignment = {
+                "course_name":course_name,
+                "assignment":info['name'],
+                "submitted": 'Yes' if info['submission']['workflow_state'] in ['graded', 'submitted', 'pending_review'] else 'No',
+                "link":info['html_url'],
+                "sync_id":f'canvas:{course_id}:{info['id']}', 
+                "source":'canvas',
+                "due_date_utc":info['due_at'],
+                "updated_at":info['updated_at']
+                }
+            assignments.append(assignment)
+    
+    return assignments
+        
+
+
+ 
 courses = get_courses(CANVAS_URL, CANVAS_TOKEN)
 
-courses_names = {course['id']:course['name'] for course in courses}
-
-print(courses_names)
-print(get_assignements(CANVAS_URL,courses[0]['id'],CANVAS_TOKEN))
+print(courses)
+print(get_assignments(CANVAS_URL, courses, CANVAS_TOKEN))
